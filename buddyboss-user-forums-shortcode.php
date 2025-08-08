e0bb489bad0be69a30772ebfb211264a96ea15cf
hananqureshi93 <133143720+hananqureshi93@users.noreply.github.com>
2025-08-09 02:21:18 +0500

    Add files via upload

---
+++ buddyboss-user-forums-shortcode.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: BuddyBoss User Forums Shortcode
+ * Description: Displays a list of forums the current user can view (via bbPress/BuddyBoss). Shortcode: [bb_user_forums].
+ * Version: 1.0.0
+ * Author: Hanan Qureshi
+ * License: MIT
+ */
+
