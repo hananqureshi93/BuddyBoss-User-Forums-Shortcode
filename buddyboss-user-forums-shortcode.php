1ad15166518eeaeeaedd5f758a9eb3600cfd0353
hananqureshi93 <133143720+hananqureshi93@users.noreply.github.com>
2025-08-09 02:22:18 +0500

    Update buddyboss-user-forums-shortcode.php

--- buddyboss-user-forums-shortcode.php
+++ buddyboss-user-forums-shortcode.php
@@ -6,4 +6,44 @@
  * Author: Hanan Qureshi
  * License: MIT
  */
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function bbufs_is_bbpress_active() {
+    return function_exists('bbp_is_forum') || class_exists('BuddyBoss_Theme');
+}
+
+function bbufs_user_can_view_forum( $forum_id ) {
+    if ( function_exists('bbp_user_can_view_forum') ) {
+        return bbp_user_can_view_forum( $forum_id );
+    }
+    // Fallback: check published & public
+    return get_post_status( $forum_id ) === 'publish';
+}
+
+function bbufs_get_user_forums( $user_id ) {
+    $args = array(
+        'post_type'      => 'forum',
+        'post_status'    => 'publish',
+        'posts_per_page' => 200,
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+        'fields'         => 'ids'
+    );
+    $forums = get_posts( $args );
+    $visible = array();
+    foreach ( $forums as $fid ) {
+        if ( bbufs_user_can_view_forum( $fid ) ) {
+            $visible[] = $fid;
+        }
+    }
+    return $visible;
+}
+
+function bbufs_shortcode( $atts ) {
+    if ( ! is_user_logged_in() ) {
+        return '<div class="bbufs-notice">Please log in to view your forums.</div>';
+    }
+    if ( ! bbufs_is_bbpress_active() ) {
+        return '<div class="bbufs-notice">bbPress / BuddyBoss Forums not detected.</div>';
+    }
 
